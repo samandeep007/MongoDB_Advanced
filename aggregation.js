cfaae43dda21eebb25dedbeb5d3e0000224696c9
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-21 18:28:35 -0600

    refactor: Update aggregation.js to count the number of users with a phone number starting with +1 (940)

--- aggregation.js
+++ aggregation.js
@@ -208,3 +208,15 @@ db.users.aggregate(
 ]
 
 
+// How many users have a phone number starting with +1 (940)
+
+[
+    {
+      $match: {
+        "company.phone": /^\+1 \(940\)/
+        }
+      }
+    ,{
+      $count: 'numberOfUsers'
+    }
+  ]
