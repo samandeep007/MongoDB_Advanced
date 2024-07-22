b51b59f74268003c21d2b2a33bcbe7f194100a20
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-21 22:49:07 -0600

    refactor: Update aggregation.js to include author details in user aggregation

--- aggregation.js
+++ aggregation.js
@@ -309,3 +309,23 @@ db.users.aggregate(
       }
     }
   ]
+
+  //Alternative
+
+  [
+    {
+      $lookup: {
+        from: "authors",
+        localField: "author_id",
+        foreignField: "_id",
+        as: "author_details"
+      }
+    }, 
+    {
+      $addFields: {
+        author_details: {
+          $arrayElemAt: ["$author_details", 0]
+        }
+      }
+    }
+  ]
